a04acf617f7bac64a7ee0fb92cebd8270a65a5d2
Karolína Vyskočilová <karolina@kybernaut.cz>
2022-01-31 18:06:05 +0000

    Fluent Forms

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
-# 0.1.0
+## 0.1.1
+
+* FluentForms - do_shortcode in Terms & Conditions
+
+## 0.1.0
 
 * Initial release (jsmprf version).
---
+++ src/Setup/FluentForms.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace KBNT\Framework\Setups;
+
+use KBNT\Framework\Interfaces\SetupInterface;
+
+class FluentForms implements SetupInterface {
+
+    /**
+     * Do shortcode in Terms & Conditions
+     * @var bool
+     */
+    private $do_shortcode_tnc;
+
+    /**
+     * Initialize.
+     * @return void
+     */
+    function init() {
+
+        if ($this->do_shortcode_tnc) {
+            add_filter('fluentform_rendering_field_data_terms_and_condition', function ($data, $form) {
+                $data['settings']['tnc_html'] = \do_shortcode($data['settings']['tnc_html']);
+                return $data;
+            }, 10, 3);
+        }
+
+    }
+
+
+    /**
+     * Set do shortcode in Terms & Conditions
+     *
+     * @param  bool  $do_shortcode_tnc  Do shortcode in Terms & Conditions
+     *
+     * @return  self
+     */
+    public function setDoShortcodeTnc(bool $do_shortcode_tnc)
+    {
+        $this->do_shortcode_tnc = $do_shortcode_tnc;
+
+        return $this;
+    }
+}
